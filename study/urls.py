d287c97958a899de48df6a872fd83375fdad836b
[mytanyhap] <[tpeople5@uncc.edu]>
2024-09-24 14:03:40 -0400

    push

--- study/urls.py
+++ study/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', admin.site.urls),
-    path('admin/',admin.site.urls),
+    path('', include('base.urls'))
 ]
